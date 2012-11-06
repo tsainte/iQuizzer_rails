4f5eef65224e89aab11f02806afa62974f48f180
Tiago Bencardino <tiagobencardino@gmail.com>
2012-11-06 14:40:12 -0300

    Inseridas rotas JSON / Rest para Quiz

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,46 +1,75 @@
 class QuizzesController < ApplicationController
+  # GET /quizzes
+  # GET /quizzes.json
   def index
     @quizzes = Quiz.all
-  end
-  
-  def show
-    @quiz = Quiz.find(params[:id])
-  end
-  
-  def new 
-    @quiz = Quiz.new
-  end
-  
-  def create
-    print "createeeeeeeeeeeeeeee"
-    @quiz = Quiz.new(params[:quiz])
-    if @quiz.save
-      redirect_to @quiz, notice: "Sucesso!"
-    else
-     # render action: "new"
-     render :new
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @quizzes }
     end
   end
   
+  # GET /quizzes/1
+  # GET /quizzes/1.json
+  def show
+    @quiz = Quiz.find(params[:id])
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @quiz }
+    end
+  end
+  
+  # GET /quizzes/new
+  # GET /quizzes/new.json
+  def new 
+    @quiz = Quiz.new
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @quiz }
+    end
+  end
+  
+  # POST /quizzes
+  # POST /quizzes.json
+  def create
+    @quiz = Quiz.new(params[:quiz])
+    if @quiz.save
+      format.html { redirect_to @quiz, :notice => 'Quiz was successfully created.' }
+      format.json { render :json => @quiz, :status => :created, :location => @quiz }
+    else
+     # render action: "new"
+     format.html { render :new }
+     format.json { render :json => @quiz.errors, :status => :unprocessable_entity }
+    end
+  end
+  
+  # GET /quizzes/1/edit
   def edit
     @quiz = Quiz.find(params[:id]) 
   end
   
+  # PUT /quizzes/1
+  # PUT /quizzes/1.json
   def update
     @quiz = Quiz.find(params[:id])
     if (@quiz.update_attributes(params[:quiz]))
       #redirect_to(action: "show", id: @quiz)
       redirect_to @quiz, notice: "Sucesso!"
     else
-      render action: "edit"
-      #render :edit
+      render :edit
     end
   end
   
+  # DELETE /quizzes/1
+  # DELETE /quizzes/1.json
   def destroy
     @quiz = Quiz.find(params[:id])
     @quiz.destroy
     
-    redirect_to(action: "index")
+    #redirect_to(action: "index")
+    respond_to do |format|
+      format.html { redirect_to quizzes_url }
+      format.json { head :no_content }
+    end
   end
 end
