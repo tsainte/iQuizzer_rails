ee29c596c52191afc2393c044c0b0cda7631bcd2
Tiago Bencardino <tiagobencardino@gmail.com>
2012-12-06 12:25:36 -0300

    removendo migrate repetido

--- db/migrate/20121102143313_create_resultados_pergunta.rb
+++
@@ -1,9 +0,0 @@
-class CreateResultadosPergunta < ActiveRecord::Migration
-  def change
-    create_table :resultados_pergunta do |t|
-      t.boolean :acertou
-
-      t.timestamps
-    end
-  end
-end
